8dc647bf49405560d950f5d47e27411c7745233a
Amadeu Arderiu <amadeu@sirvelia.com>
2024-09-24 08:48:57 +0200

    assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-data', 'wp-i18n'), 'version' => '3b04a09bf4597162d63a');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-data', 'wp-i18n'), 'version' => '9a5f34d6856780bbe42b');
